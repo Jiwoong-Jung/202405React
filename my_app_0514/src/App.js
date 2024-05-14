76b48ed7d497d06bc6efe6fb59c0a6f51c2bb311
Jiwoong Jung <jjw981119@naver.com>
2024-05-14 10:57:29 +0900

    first commit

---
+++ my_app_0514/src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import Potato from './Potato';
+
+function Potato2() {
+  return <h2>Potato22222!!</h2>;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello!!</h1>
+      <h1>Hello!!</h1>
+      <h1>Hello!!</h1>
+      <Potato2 />
+      <Potato />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ my_app_0514/src/Potato.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Potato() {
+  return <h2>Potato!!</h2>;
+}
+
+export default Potato;
